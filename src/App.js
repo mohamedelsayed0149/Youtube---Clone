d44bc3b7b929d5fd0179a8c7773cb762283d88eb
mohamedelsayed0149 <mohamed11490@gmail.com>
2021-09-26 14:11:12 +0200

    Youtube Clone

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Main/Header'
+import Content from './Main/Content'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
